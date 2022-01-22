0744c745a8935b81eaeea4884ce4f752fb48fc85
davidjj76 <davidjj76@gmail.com>
2022-01-22 20:05:12 +0100

    Redux tests

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -3,9 +3,9 @@ export const getIsLogged = state => state.auth;
 export const getUi = state => state.ui;
 
 export const getAreTagsLoaded = state => getTags(state).length > 0;
+export const getTags = state => state.tags;
+
+export const getAreAdvertsLoaded = state => state.adverts.loaded;
 export const getAdverts = state => state.adverts.data;
 export const getAdvert = (state, advertId) =>
   getAdverts(state).find(advert => advert.id === advertId);
-export const getAreAdvertsLoaded = state => state.adverts.loaded;
-
-export const getTags = state => state.tags;
